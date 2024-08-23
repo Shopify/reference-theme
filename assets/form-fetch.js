6ca019297541fbe83806fd3bceeb57d4693da6ce
André Neder <andre@beeclever.de>
2024-08-23 20:11:50 +0200

    test

---
+++ assets/form-fetch.js
@@ -0,0 +1,34 @@
+
+
+class FormFetch extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.form = this.querySelector('form')
+
+    this.form.addEventListener('submit', e => {
+      e.preventDefault();
+      
+      const formData = new FormData(this.form)
+
+      fetch(this.form.action, {
+        method: this.form.method, 
+        body: formData
+      })
+      .then(res => res.json())
+      .then(console.log)
+      .catch(console.error)
+    })
+  }
+  
+  static get observedAttributes() {
+    return [];
+  }
+  attributeChangedCallback(property, oldValue, newValue) {
+    if (oldValue === newValue) return;
+
+    this[ property ] = newValue;
+  }
+}
+customElements.define('form-fetch', FormFetch);
