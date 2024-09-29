401faefff8a46d0bbac75c8dbac2ab86ffe1c16d
Benedikt Krieger <benedikt@beeclever.de>
2024-09-29 15:44:18 +0200

    product-main

--- assets/product-form.js
+++ assets/product-form.js
@@ -1,10 +1,34 @@
+class ProductOptionValue extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    const form = this.closest('form');
+    const variantId = this.getAttribute('variant-id');
+    const productUrl = this.getAttribute('product-url');
+    if(!!form){
+      const variantIdInput = form.querySelector(`[name="id"]`);
+      const productUrlInput = form.querySelector(`[name="url"]`);
+      this.addEventListener('click', () => {
+        productUrlInput?.setAttribute('value', productUrl);
+        variantIdInput?.setAttribute('value', variantId);
+      });
+    }
+  }
+}
+customElements.define("product-option-value", ProductOptionValue);
+
 class ProductForm extends HTMLElement {
   constructor() {
     super();
   }
   connectedCallback() {
-    const variantIdInputs = this.querySelectorAll(`[name="id"]`);
-    const sellingPlanInputs = this.querySelectorAll(`[name="selling_plan"]`);
+    const productUrlInput = this.querySelector(`[name="id"]`);
+    const variantIdInput = this.querySelector(`[name="id"]`);
+    const sellingPlanInput = this.querySelector(`[name="selling_plan"]`);
+    variantIdInput.addEventListener('input', () => {
+      console.log(productUrlInput?.value, variantIdInput?.value, sellingPlanInput?.value);
+    });
   }
 }
 customElements.define("product-form", ProductForm);
